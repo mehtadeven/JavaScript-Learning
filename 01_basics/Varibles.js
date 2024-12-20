b44a3b31508f2620dd2ade49f37211bdc68c1d86
Mehta Deven <devenmehta2@gmail.com>
2024-12-20 10:47:27 +0000

    #

---
+++ 01_basics/Varibles.js
@@ -0,0 +1,9 @@
+const accountId = 439999
+let accountEmail = "devenmehta@gmail.com"
+var accpassword = 1213
+let accountState = null;
+// console.log(accountId) 
+// console.log(accountEmail)
+// console.log(accpassword)
+
+console.table([accountId,accountEmail,accpassword,accountState]);
