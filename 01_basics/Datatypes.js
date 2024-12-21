9b1b1ab2017521ba903d4b0e3c6d66fccc9d4383
Mehta Deven <devenmehta2@gmail.com>
2024-12-21 06:39:05 +0000

    Datatypes

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+let name = "Deven"
+let age = 34
+let isLoggegin = false
+
+// number 
+// biginit
+// String => "",'',``
+// Boolean => true/Fase
+// null => Standalone value 
+// undefined =>  
+// symbol => unique
+// object 
+
+console.log(typeof undefined); // Undifined
+console.log(typeof null); //object
+
