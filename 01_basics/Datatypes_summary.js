5401c496c8331e6f2ba49ae315b75c4be2685308
Mehta Deven <devenmehta2@gmail.com>
2024-12-22 06:38:56 +0000

    Primitive_Summary

---
+++ 01_basics/Datatypes_summary.js
@@ -0,0 +1,30 @@
+// Primitive & Non-Primitive
+
+// 7 Types of Primitive DataType
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.33;
+
+const isLoggedIn = false
+const outsidetemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 34353454521324242n
+// console.log(typeof bigNumber);
+
+//=======Refrence (Non-Primitive)=========
+
+// Array, Object, Functions
+
+const heros =["Ironman","Superman","Moneyman"]
+let obj1 = { 
+    name: "Deven",
+    age : 31,
+    IsLoggedIn: false,
+}
