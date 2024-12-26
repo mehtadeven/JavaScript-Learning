d74993b31e43574eaff9e2be7d89526b3d6c5612
Mehta Deven <devenmehta2@gmail.com>
2024-12-26 08:07:29 +0000

    Date & Time

---
+++ 01_basics/Dates.js
@@ -0,0 +1,8 @@
+// Dates
+
+let myDate = new Date()
+//? console.log(myDate.toString()); // Thu Dec 26 2024 08:01:32 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); // Thu Dec 26 2024
+// console.log(myDate.toJSON()); // 2024-12-26T08:00:55.716Z
+// console.log(myDate.toLocaleDateString()); // 12/26/2024
+// 
