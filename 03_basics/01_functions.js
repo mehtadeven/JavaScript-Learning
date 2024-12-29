15932be3d0f750e548eb5a391e30f2038a2289e3
Mehta Deven <devenmehta2@gmail.com>
2024-12-29 09:35:03 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,43 +1,69 @@
-// console.log("D");
-// console.log("E");
-// console.log("V");
-// console.log("E");
-// console.log("N");
-
-function myName() {
-  console.log("D");
-  console.log("E");
-  console.log("V");
-  console.log("E");
-  console.log("N");
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
 }
 
-// myName()
+// sayMyName()
 
-// function sumNumber ( num1, num2){
-//     console.log(num1 + num2);
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
 // }
 
-function sumNumber ( num1, num2){
-    
-    // let result = num1 + num2
-    // return result;
+function addTwoNumbers(number1, number2){
 
-    return num1 + num2
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
 }
 
-const result = sumNumber(2, 3)
+const result = addTwoNumbers(3, 5)
 
-// console.log("Result :",result);
+// console.log("Result: ", result);
 
-function loginUserMessage(username) {
-    if (username === undefined) {
-        // We can check the Upper values By (!username)
-        console.log("Please Enter the UserName");
-        return        
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
     }
     return `${username} just logged in`
 }
 
-// console.log(loginUserMessage("Deven"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
