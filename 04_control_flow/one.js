46ca1da1da2fb9d5cfaad5df3ab49f90bec26bb1
Mehta Deven <devenmehta2@gmail.com>
2025-01-08 02:29:14 +0000

    control flow code file.

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+// if
+const isUser = true 
+const temperature = 13
+
+// if (temperature === 13) {
+//     console.log("less than 50");
+// }else{
+//     console.log("temprature is greater than 50");
+// }
+
+// console.log("Executed");
+//! <, >, <=, >=, ==, =!
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power :${power}`);
+// }
+
+// console.log(`User Power :${score}`);
+
