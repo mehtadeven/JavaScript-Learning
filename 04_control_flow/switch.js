ca2859c88212c8f5d97e024340581fcfbd5feaec
Mehta Deven <devenmehta2@gmail.com>
2025-01-11 02:54:00 +0000

    control flow code files

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+        
+    default:
+        console.log("Not Matched");
+        break;
+}
