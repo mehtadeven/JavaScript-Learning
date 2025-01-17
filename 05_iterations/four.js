64aba191b8d7095ac5aae90b1bb2422a89e94688
Mehta Deven <devenmehta2@gmail.com>
2025-01-17 06:58:35 +0000

    loops discussion

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObj ={
+    js : "JavaScript",
+    cpp: "C++",
+    rb : "Ruby",
+    swift : "Swift",
+}
+
+for (const key in myObj) {
+    // console.log(`${key} shortcut for ${myObj[key]}`);
+}
+
+const Programming = ["js", "py", "java", "rb", "cpp"]
+
+for (const key in Programming) {
+    // console.log(Programming[key]);
+}
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "United states of Americea")
+// map.set('FR', "Franch")
+
+// for (const key in map) {
+//     console.log(key);    
+// }
+
